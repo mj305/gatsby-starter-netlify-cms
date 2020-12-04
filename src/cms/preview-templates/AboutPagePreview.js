f96a547768294c3dcc02dd04a56dbc24bda7014e
Cheng <fazylalmeeve@gmail.com>
2020-12-03 18:05:40 -0800

    update about page

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,18 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
-  />
-)
+const AboutPagePreview = ({ entry, getAsset }) => {
+  const data = entry.getIn(['data']).toJS()
+
+  if (data) {
+    return (
+      <AboutPageTemplate
+        image={getAsset(data.image)}
+        title={data.title}
+        subTitle={data.subTitle}
+        boxTitle={data.subheading}
+        boxContent={data.boxContent}
+        images={data.images || {}}
+        main={data.main || {}}
+        wedo={data.wedo || {}}
+        bottom={data.bottom || {}}
+      />
+    )
+  } else {
+    return <div>Loading...</div>
+  }
+}
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  widgetFor: PropTypes.func,
+  getAsset: PropTypes.func,
 }
 
 export default AboutPagePreview
